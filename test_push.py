2726bb784fca13305850e87086587f9fe5f772f1
joaojfarfanv <joaojfarfanv@gmail.com>
2025-12-15 22:39:31 -0500

    Create test_push.py

---
+++ test_push.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+# TUS CREDENCIALES
+ONESIGNAL_APP_ID = "7d8ae299-535f-4bbf-a14b-28852b836721" 
+ONESIGNAL_API_KEY = "os_v2_app_pwfofgktl5f37iklfccsxa3heh4u2gairwjeqv4nendwoqymz65uqdpx3sutokduinay65ksui4zhy4vf4xwv2geu3f67d5il6hif5i"
+
+def test_notification():
+    header = {
+        "Content-Type": "application/json; charset=utf-8",
+        "Authorization": f"Key {ONESIGNAL_API_KEY}"
+    }
+    payload = {
+        "app_id": ONESIGNAL_APP_ID,
+        "headings": {"en": "🔔 Test de Prueba"},
+        "contents": {"en": "Si lees esto, ¡funciona correctamente!"},
+        "included_segments": ["Total Subscriptions"]
+    }
+    
+    req = requests.post("https://onesignal.com/api/v1/notifications", headers=header, data=json.dumps(payload))
+    print(f"Estado: {req.status_code}")
+    print(req.text)
+
+if __name__ == "__main__":
+    test_notification()
